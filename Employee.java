288b780b7699d44cbef350555f075b3d906b6863
Ashish Dev <ashishdevofficial@outlook.com>
2019-11-26 18:54:48 -0600

    Close connection in Department class and add initializeProjects method in Employee class

--- Employee.java
+++ Employee.java
@@ -1,14 +1,40 @@
+import java.sql.*;
+import java.util.List;
+
 public class Employee{
     private String fName;
     private String mInit;
     private String lName;
     private int ssn;
+    private List<Project> projects;
 
     public Employee(String fName, String mInit, String lName, int ssn){
         this.fName = fName;
         this.mInit = mInit;
         this.lName = lName;
-        this.ssn = ssn;        
+        this.ssn = ssn;    
+        
+        initializeProjects();
+    }
+
+
+    private void initializeProjects(){
+
+        MakeConnection iconnect = new MakeConnection("deva.txt");
+        Connection connection = iconnect.gConnection();
+
+        ResultSet projectSet = new QueryParser(connection).getResult("SELECT * FROM WORKS_ON JOIN PROJECT ON Pnumber=Pno WHERE Essn=" + this.ssn);
+
+        try {
+
+            while (projectSet.next()) {
+                // this.projects.add(new Project(projectSet.getString("Pname"), projectSet.getInt("Pnumber"), projectSet.getInt("Dnum")));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Couldn't initilaize projects in Employee");
+            e.printStackTrace();
+        }
     }
 
     public int compareTo(Employee e){
