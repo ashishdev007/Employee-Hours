431693993b771131864e28370bc66559ce64f8e1
Ashish Dev <ashishdevofficial@outlook.com>
2019-11-26 19:41:18 -0600

    Create the Report class which will generate the report

---
+++ Report.java
@@ -0,0 +1,40 @@
+import java.sql.ResultSet;
+
+public class Report{
+
+    private List<Department> departments;
+
+    public Report(){
+        departments = new ArrayList<>();
+        initializeDepartments();
+    }
+
+    private void initializeDepartments(){
+        MakeConnection iconnect = new MakeConnection("deva.txt");
+        Connection connection = iconnect.gConnection();
+
+
+        try{
+            if(!connection.isClosed() && connection != null){
+                ResultSet depts = new QueryParser(connection).getResult("SELECT Dname, Dnumber FROM DEPARTMENT ORDER BY Dnumber");
+    
+                while (depts.next()){
+                    departments.add(new Department(depts.getString("Dname"), depts.getInt("Dnumber")));
+                }
+
+            } 
+            else{
+                System.out.println("Connection is Closed or it's null.");
+            }
+        }
+        catch(SQLException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public void generateReport(){
+        for (Department department : departments) {
+            System.out.println(department.getEmployeeHrs());
+        }
+    }
+}
